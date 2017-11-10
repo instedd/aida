352f6a54cd6ca99b8cf14ab7a560b1e14d81ab27
Mariano Coca <marianoabelcoca@gmail.com>
2017-11-10 18:25:09 -0300

    Add .flowconfig and pre-commit script
    Remove App.test.js scaffold

---
+++ pre-commit.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+echo "Running Mix tests"
+MIX_TESTS="$(docker-compose run --rm app mix test)"
+
+if [ $? -eq 0 ]; then
+   echo "OK";
+else
+  echo "${MIX_TESTS}"
+fi
+
+echo "Running JS tests"
+JS_TESTS="$(docker-compose run --rm -e CI=true node yarn test)"
+
+if [ $? -eq 0 ]; then
+  echo "OK";
+else
+  echo "${JS_TESTS}"
+fi
+
+echo "Running Flow tests"
+if hash flow 2>/dev/null; then
+  FLOW_TESTS="$(flow check)"
+else
+  FLOW_TESTS="$(./assets/aida_web/node_modules/.bin/flow check)"
+fi
+
+if [ $? -eq 0 ]; then
+  echo "OK";
+else
+  echo "${FLOW_TESTS}"
+fi
+
+echo "Running Eslint tests"
+ESLINT_TESTS="$(docker-compose run --rm node yarn eslint)"
+
+if [ $? -eq 0 ]; then
+  echo "OK";
+else
+  echo "${ESLINT_TESTS}"
+fi
+
+echo "Running Dialyzer"
+DIALYZER_TESTS="$(docker-compose run --rm app mix dialyzer)"
+
+if [ $? -eq 0 ]; then
+  echo "OK";
+else
+  echo "${DIALYZER_TESTS}"
+fi
