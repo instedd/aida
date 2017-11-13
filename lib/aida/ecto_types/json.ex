ba33bba4d49a36c7915509a9a10f87295f84283f
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-11-13 18:31:37 -0300

    Fix bot creation from UI: avoid double JSON encoding

--- lib/aida/ecto_types/json.ex
+++ lib/aida/ecto_types/json.ex
@@ -1,6 +1,7 @@
 defmodule Aida.Ecto.Type.JSON do
   @behaviour Ecto.Type
   def type, do: :text
+  def cast(string) when is_binary(string), do: Poison.decode(string)
   def cast(any), do: {:ok, any}
   def load(string) when is_binary(string), do: Poison.decode(string)
   def dump(json), do: Poison.encode(json)
