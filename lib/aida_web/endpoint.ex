ef4662103c9855d74cec1eac03c86987e57b5d04
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-11-14 16:53:17 -0300

    Serve static build of react app

--- lib/aida_web/endpoint.ex
+++ lib/aida_web/endpoint.ex
@@ -8,8 +8,7 @@ defmodule AidaWeb.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :aida, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    at: "/", from: :aida, gzip: false
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
