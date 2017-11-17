ac6ead18cdb5bfb7177c3795e33e38d961498444
Juan F. Blandini <jblandini@manas.com.ar>
2017-11-17 19:24:00 -0300

    Changes to the pre-commit.sh, to only show the dialyzer output when finding errors

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Running Mix tests"
+echo "----==== Running Mix tests ====----"
 MIX_TESTS="$(docker-compose run --rm app mix test)"
 
 NC='\033[0m'
@@ -11,7 +11,7 @@ else
   echo "${MIX_TESTS}"
 fi
 
-echo "Running JS tests"
+echo "----==== Running JS tests ====----"
 JS_TESTS="$(docker-compose run --rm -e CI=true node yarn test)"
 
 if [ $? -eq 0 ]; then
@@ -20,7 +20,7 @@ else
   echo "${JS_TESTS}"
 fi
 
-echo "Running Flow tests"
+echo "----==== Running Flow tests ====----"
 if hash flow 2>/dev/null; then
   FLOW_TESTS="$(flow check)"
 else
@@ -33,7 +33,7 @@ else
   echo "${FLOW_TESTS}"
 fi
 
-echo "Running Eslint tests"
+echo "----==== Running Eslint tests ====----"
 ESLINT_TESTS="$(docker-compose run --rm node yarn eslint)"
 
 if [ $? -eq 0 ]; then
@@ -42,5 +42,13 @@ else
   echo "${ESLINT_TESTS}"
 fi
 
-echo "Running Dialyzer"
-docker-compose run --rm app mix dialyzer
+echo "----==== Running Dialyzer ====----"
+
+DIALYZER_TESTS="$(docker-compose run --rm app mix dialyzer)"
+
+if [[ $DIALYZER_TESTS == *"done (passed successfully)"* ]]; then
+  echo "${GREEN}OK${NC}";
+else
+  echo "${DIALYZER_TESTS}"
+fi
+
