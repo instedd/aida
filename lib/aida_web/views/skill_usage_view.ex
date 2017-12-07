0192603122ac0b5bf59796c011b4a3f20888d9e2
Juan F. Blandini <jblandini@manas.com.ar>
2017-12-07 14:18:30 -0300

    Modifying the dummy Stats API with some suggested changes. WIP for #20 and #22

--- lib/aida_web/views/skill_usage_view.ex
+++ lib/aida_web/views/skill_usage_view.ex
@@ -2,12 +2,26 @@ defmodule AidaWeb.SkillUsageView do
   use AidaWeb, :view
   alias AidaWeb.SkillUsageView
 
-  def render("users_per_period.json", %{skill_usages: skill_usages}) do
-    %{data: render_many(skill_usages, SkillUsageView, "user.json")}
+  def render("users_per_period.json", %{users_count: users_count}) do
+    %{active_users: users_count}
   end
 
-  def render("users_per_skill.json", %{skill_usages: skill_usages}) do
-    %{data: render_many(skill_usages, SkillUsageView, "user.json")}
+  def render("users_per_skill.json", %{skills: skills}) do
+    render_many(skills, SkillUsageView, "usage_count.json")
+  end
+
+  # def render("index.json", %{skill_usages: skill_usages}) do
+  #   %{data: render_many(skill_usages, SkillUsageView, "skill_usage.json")}
+  # end
+
+  # def render("show.json", %{skill_usage: skill_usage}) do
+  #   %{data: render_one(skill_usage, SkillUsageView, "skill_usage.json")}
+  # end
+
+  def render("usage_count.json", %{skill_usage: skill_usage}) do
+    %{skill_id: skill_usage.skill_id,
+      count: skill_usage.count
+    }
   end
 
   def render("user.json", %{skill_usage: skill_usage}) do
@@ -17,4 +31,12 @@ defmodule AidaWeb.SkillUsageView do
     }
   end
 
+  # def render("skill_usage.json", %{skill_usage: skill_usage}) do
+  #   %{bot_id: skill_usage.bot_id,
+  #     user_id: skill_usage.user_id,
+  #     last_usage: skill_usage.last_usage,
+  #     skill_id: skill_usage.skill_id,
+  #     user_generated: skill_usage.user_generated
+  #   }
+  # end
 end
