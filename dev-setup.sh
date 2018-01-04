7858bbd4df0da6a87d01287cad7778d766db5166
Juan Wajnerman <jwajnerman@manas.com.ar>
2018-01-04 18:23:14 -0300

    Added ui to the development environment

--- dev-setup.sh
+++ dev-setup.sh
@@ -3,3 +3,4 @@ docker-compose build
 docker-compose run --rm app mix deps.get
 docker-compose run --rm app mix ecto.setup
 docker-compose run --rm node yarn install
+docker-compose run --rm ui /bin/sh -c 'rake db:setup || rake db:migrate'
