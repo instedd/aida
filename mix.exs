16f252394a24e39121cd377377fe8e4c9628f1e8
Juan Wajnerman <jwajnerman@manas.com.ar>
2018-01-10 14:34:20 -0300

    Version bump (0.5.0)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Aida.Mixfile do
   def project do
     [
       app: :aida,
-      version: "0.4.0",
+      version: "0.5.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       erlc_paths: elixirc_paths(Mix.env),
