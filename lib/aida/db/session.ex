0209e93d80c50755a98ca5a2af05ad85f542abe1
Leandro Matayoshi <lmatayoshi@manas.com.ar>
2018-01-10 16:20:11 -0300

    #47. Remove unnecessary assignment in db/session.ex

--- lib/aida/db/session.ex
+++ lib/aida/db/session.ex
@@ -16,21 +16,19 @@ defmodule Aida.DB.Session do
   end
 
   def session_index_by_bot(bot_id) do
-    sessions = Session 
-            |> join(:inner, [s], m in MessageLog, m.session_id == s.id) 
-            |> where([s], like(s.id, ^"#{bot_id}/%"))
-            |> group_by([s, m], s.id)
-            |> select([s, m], %{id: s.id, first_message: min(m.inserted_at), last_message: max(m.inserted_at)})
-            |> Repo.all()
-    sessions
+    Session 
+      |> join(:inner, [s], m in MessageLog, m.session_id == s.id) 
+      |> where([s], like(s.id, ^"#{bot_id}/%"))
+      |> group_by([s, m], s.id)
+      |> select([s, m], %{id: s.id, first_message: min(m.inserted_at), last_message: max(m.inserted_at)})
+      |> Repo.all()
   end
 
   def message_logs_by_session(session_id) do
-    logs = MessageLog 
-        |> where([m], m.session_id == ^session_id)
-        |> Repo.all
-        |> Enum.map(&(%{timestamp: &1.inserted_at, direction: &1.direction, content: &1.content}))
-    logs
+    MessageLog 
+      |> where([m], m.session_id == ^session_id)
+      |> Repo.all
+      |> Enum.map(&(%{timestamp: &1.inserted_at, direction: &1.direction, content: &1.content}))
   end
 
   @doc false
