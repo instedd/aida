6de809c27adebe43c6cd068337cbbaeee1b8b3fc
Leandro Matayoshi <lmatayoshi@manas.com.ar>
2018-01-12 17:02:02 -0300

    Fixes #54. Trigger message to a specific session

--- lib/aida_web/controllers/session_controller.ex
+++ lib/aida_web/controllers/session_controller.ex
@@ -1,7 +1,6 @@
 defmodule AidaWeb.SessionController do
   use AidaWeb, :controller
-  alias Aida.DB
-  alias Aida.DB.Session
+  alias Aida.{DB, DB.Session, ChannelProvider, Channel}
 
   def index(conn, %{"bot_id" => bot_id}) do
     sessions = Session.session_index_by_bot(bot_id)
@@ -16,6 +15,13 @@ defmodule AidaWeb.SessionController do
   def log(conn, %{"session_id" => session_id}) do
     logs = Session.message_logs_by_session(session_id)
     render(conn, "logs.json", logs: logs)
+  end
+
+  def send_message(conn, %{"session_id" => session_id, "message" => message}) do
+    ChannelProvider.find_channel(session_id)
+    |> Channel.send_message([message], session_id)
+    
+    conn |> send_resp(200, "") 
   end
 
 end
