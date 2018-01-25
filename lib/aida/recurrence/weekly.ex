b8883dbbe6122f6d494cbd5b3b042704170d317e
Juan Wajnerman <jwajnerman@manas.com.ar>
2018-01-25 00:33:49 -0300

    Implementation of weekly recurrences

--- lib/aida/recurrence/weekly.ex
+++ lib/aida/recurrence/weekly.ex
@@ -13,8 +13,82 @@ defmodule Aida.Recurrence.Weekly do
             on: []
 
   defimpl Aida.Recurrence, for: __MODULE__ do
-    def next(%Weekly{}, now) do
-      now
+    @seconds_in_a_day 86_400
+    @seconds_in_a_week 604_800
+
+    def next(%Weekly{start: start, on: on, every: every}, now) do
+      if DateTime.compare(start, now) == :gt do
+        # We're before the start date of the recurrence
+        # Just adjust forward the date to one of the allowed days of week
+        start
+        |> adjust_day_of_week(on)
+      else
+        # Calculate the number of whole days between the start date and now
+        diff_in_days = DateTime.diff(now, start) |> div(@seconds_in_a_day)
+
+        start
+        # This will select the current date at the start time if the current time
+        # is before the start time, or the next day at start time otherwise
+        |> Timex.shift(days: diff_in_days + 1)
+        # Choose the next available day of week
+        |> adjust_day_of_week(on)
+        # Finally, move to the next enabled week (if `every` is greater than 1)
+        |> adjust_every(start, every)
+      end
     end
+
+    # Move the datetime to the next enabled week. Does nothing if `every = 1`.
+    # Note that actually the special case for `every = 1` is not necessary but
+    # added just to avoid calculations.
+    defp adjust_every(datetime, _, 1), do: datetime
+    defp adjust_every(datetime, start, every) do
+      # Calculate the number of whole weeks between the start date and the given date
+      diff_in_weeks = DateTime.diff(datetime, start) |> div(@seconds_in_a_week)
+
+      # Make the number of whole weeks a multiple of the `every` value
+      case rem(diff_in_weeks, every) do
+        0 ->
+          # Already a multiple, no changes
+          datetime
+        rem ->
+          Timex.shift(datetime, weeks: every - rem)
+      end
+    end
+
+    # Moves forward the date to make it fall into one of the available days of week
+    defp adjust_day_of_week(datetime, on) do
+      # Get the selected day of week as an integer (1 - Monday, ..., 7 - Sunday)
+      dow =
+        datetime
+        |> DateTime.to_date
+        |> Date.day_of_week
+
+      # Calculate the necessary offset and move forward the date
+      offset = adjust_offset(dow, on)
+      Timex.shift(datetime, days: offset)
+    end
+
+    # Calculate the offset in days between a day of week and the next available one
+    # It works by moving to the next day of week until it succeeds (worst case: 6 iterations)
+    defp adjust_offset(dow, on, offset \\ 0)
+    defp adjust_offset(_, on, offset) when offset >= 7, do: raise "Invalid 'on' value for weekly recurrence: #{inspect on}"
+    defp adjust_offset(dow, on, offset) do
+      if dow_to_atom(dow) in on do
+        offset
+      else
+        adjust_offset(next_dow(dow), on, offset + 1)
+      end
+    end
+
+    defp dow_to_atom(1), do: :monday
+    defp dow_to_atom(2), do: :tuesday
+    defp dow_to_atom(3), do: :wednesday
+    defp dow_to_atom(4), do: :thursday
+    defp dow_to_atom(5), do: :friday
+    defp dow_to_atom(6), do: :saturday
+    defp dow_to_atom(7), do: :sunday
+
+    defp next_dow(7), do: 1
+    defp next_dow(dow), do: dow + 1
   end
 end
--- test/aida/recurrence_test.exs
+++ test/aida/recurrence_test.exs
@@ -1,6 +1,6 @@
 defmodule Aida.RecurrenceTest do
   alias Aida.Recurrence
-  alias Aida.Recurrence.Daily
+  alias Aida.Recurrence.{Daily, Weekly}
   use ExUnit.Case
   use Aida.TimeMachine
 
@@ -11,33 +11,104 @@ defmodule Aida.RecurrenceTest do
     end
 
     test "with start in the future" do
-      start = ~N[2040-01-01 18:00:00] |> utc
-      assert %Daily{start: start} |> Recurrence.next == start
+      start = ~U[2040-01-01 18:00:00]
+      now = ~U[2018-01-01 10:00:00]
+      assert %Daily{start: start} |> Recurrence.next(now) == start
     end
 
     test "after the start and before the time" do
-      start = ~N[2000-01-01 18:00:00] |> utc
-      now = ~N[2018-01-05 10:00:00] |> utc
-      next = ~N[2018-01-05 18:00:00] |> utc
+      start = ~U[2000-01-01 18:00:00]
+      now = ~U[2018-01-05 10:00:00]
+      next = ~U[2018-01-05 18:00:00]
       assert %Daily{start: start} |> Recurrence.next(now) == next
     end
 
     test "after the start and after the time" do
-      start = ~N[2000-01-01 18:00:00] |> utc
-      now = ~N[2018-01-05 19:00:00] |> utc
-      next = ~N[2018-01-06 18:00:00] |> utc
+      start = ~U[2000-01-01 18:00:00]
+      now = ~U[2018-01-05 19:00:00]
+      next = ~U[2018-01-06 18:00:00]
       assert %Daily{start: start} |> Recurrence.next(now) == next
     end
 
     test "after the start every 3 days" do
-      start = ~N[2018-01-01 18:00:00] |> utc
-      now = ~N[2018-01-05 19:00:00] |> utc
-      next = ~N[2018-01-07 18:00:00] |> utc
+      start = ~U[2018-01-01 18:00:00]
+      now = ~U[2018-01-05 19:00:00]
+      next = ~U[2018-01-07 18:00:00]
       assert %Daily{start: start, every: 3} |> Recurrence.next(now) == next
     end
   end
 
-  defp utc(naive_ts) do
-    naive_ts |> DateTime.from_naive!("Etc/UTC")
+  describe "weekly recurrence" do
+    test "defaults to every 1 week" do
+      start = DateTime.utc_now
+      assert %Weekly{start: start, on: [:monday]} == %Weekly{start: start, on: [:monday], every: 1}
+    end
+
+    test "every week on a single day" do
+      recurrence = %Weekly{start: ~U[2018-01-02 18:00:00], on: [:wednesday]}
+      assert Recurrence.next(recurrence, ~U[2000-01-01 01:00:00]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-01 01:00:00]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-03 01:00:00]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-03 17:59:59]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-03 18:00:00]) == ~U[2018-01-10 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-03 18:10:00]) == ~U[2018-01-10 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-08 00:00:00]) == ~U[2018-01-10 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2818-01-01 00:00:00]) == ~U[2818-01-03 18:00:00]
+    end
+
+    test "every week, twice a week" do
+      recurrence = %Weekly{start: ~U[2018-01-02 18:00:00], on: [:wednesday, :friday]}
+      assert Recurrence.next(recurrence, ~U[2000-01-01 01:00:00]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-01 01:00:00]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-03 01:00:00]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-03 17:59:59]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-03 18:00:00]) == ~U[2018-01-05 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-04 18:00:00]) == ~U[2018-01-05 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-05 18:00:00]) == ~U[2018-01-10 18:00:00]
+    end
+
+    test "every other week on a single day" do
+      recurrence = %Weekly{start: ~U[2018-01-02 18:00:00], on: [:wednesday], every: 2}
+      assert Recurrence.next(recurrence, ~U[2000-01-01 01:00:00]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-01 01:00:00]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-03 01:00:00]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-03 17:59:59]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-03 18:00:00]) == ~U[2018-01-17 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-03 18:10:00]) == ~U[2018-01-17 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-08 00:00:00]) == ~U[2018-01-17 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-22 00:00:00]) == ~U[2018-01-31 18:00:00]
+    end
+
+    test "every other week, twice a week" do
+      recurrence = %Weekly{start: ~U[2018-01-02 18:00:00], on: [:wednesday, :friday], every: 2}
+      assert Recurrence.next(recurrence, ~U[2000-01-01 01:00:00]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-01 01:00:00]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-03 01:00:00]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-03 17:59:59]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-03 18:00:00]) == ~U[2018-01-05 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-04 18:00:00]) == ~U[2018-01-05 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-05 18:00:00]) == ~U[2018-01-17 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-18 18:00:00]) == ~U[2018-01-19 18:00:00]
+    end
+
+    test "every three weeks on a single day" do
+      recurrence = %Weekly{start: ~U[2018-01-02 18:00:00], on: [:wednesday], every: 3}
+      assert Recurrence.next(recurrence, ~U[2018-01-01 01:00:00]) == ~U[2018-01-03 18:00:00]
+      assert Recurrence.next(recurrence, ~U[2018-01-04 01:00:00]) == ~U[2018-01-24 18:00:00]
+    end
+
+    test "fails when the days of week is invalid or empty" do
+      assert_raise RuntimeError, "Invalid 'on' value for weekly recurrence: []", fn ->
+        %Weekly{start: DateTime.utc_now, on: []} |> Recurrence.next
+      end
+
+      assert_raise RuntimeError, "Invalid 'on' value for weekly recurrence: [:foo]", fn ->
+        %Weekly{start: DateTime.utc_now, on: [:foo]} |> Recurrence.next
+      end
+    end
+  end
+
+  defp sigil_U(ts, _) do
+    NaiveDateTime.from_iso8601!(ts) |> DateTime.from_naive!("Etc/UTC")
   end
 end
