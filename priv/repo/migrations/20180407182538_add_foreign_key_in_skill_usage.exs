0c1969d55e92887f5308c725ae5b3a44f925c2a7
Leandro Matayoshi <lmatayoshi@manas.com.ar>
2018-04-09 17:24:48 -0300

    #65. Remove entities with deleted sessions or bots in migrations

--- priv/repo/migrations/20180407182538_add_foreign_key_in_skill_usage.exs
+++ priv/repo/migrations/20180407182538_add_foreign_key_in_skill_usage.exs
@@ -1,7 +1,10 @@
 defmodule Aida.Repo.Migrations.AddForeignKeyInSkillUsage do
   use Ecto.Migration
+  alias Aida.Repo
 
   def change do
+    Repo.query!("DELETE FROM skill_usage WHERE bot_id NOT IN (SELECT id FROM bots)")
+
     alter table(:skill_usage) do
       modify :bot_id, references(:bots, on_delete: :delete_all, type: :binary_id)
     end
