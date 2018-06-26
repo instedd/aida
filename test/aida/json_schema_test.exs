a090de901e3e93234072045fb2b5b07dd0a8cbcf
Mariano Coca <marianoabelcoca@gmail.com>
2018-06-26 17:33:17 -0300

    Add valid_manifest_images to json_schema_tests

--- test/aida/json_schema_test.exs
+++ test/aida/json_schema_test.exs
@@ -468,6 +468,9 @@ defmodule Aida.JsonSchemaTest do
     File.read!("test/fixtures/valid_manifest_with_skill_relevances.json")
     |> assert_valid(:manifest_v1)
 
+    File.read!("test/fixtures/valid_manifest_images.json")
+    |> assert_valid(:manifest_v1)
+
     File.read!("test/fixtures/valid_manifest.json")
     |> assert_valid(:manifest_v1)
   end
