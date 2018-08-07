3cbfd423a3a720fb797b9de2939a9de129c35dcb
aeatencio <aatencio@manas.tech>
2018-08-07 13:47:56 -0300

    Support facebook callbacks for an specific bot
    And fallback to the previous behaviour when bot_id is not provided.
    
    For #167
    
    Co-authored-by: Mariano Coca <mcoca@manas.com.ar>

--- lib/aida_web/controllers/callback_controller.ex
+++ lib/aida_web/controllers/callback_controller.ex
@@ -1,6 +1,10 @@
 defmodule AidaWeb.CallbackController do
   use AidaWeb, :controller
 
+  def callback(conn, %{"provider" => "facebook", "bot_id" => bot_id}) do
+    Aida.Channel.Facebook.callback(conn, bot_id)
+  end
+
   def callback(conn, %{"provider" => "facebook"}) do
     Aida.Channel.Facebook.callback(conn)
   end
