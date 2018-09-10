14d79fd36bf83803755e37213b310f1017d66dbe
Mariano Coca <marianoabelcoca@gmail.com>
2018-09-10 19:39:21 -0300

    Bump version to 1.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Aida.Mixfile do
   def project do
     [
       app: :aida,
-      version: "1.2.0",
+      version: "1.3.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       erlc_paths: elixirc_paths(Mix.env),
